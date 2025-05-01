a98652315c24998df5a64b0594da8f610a3b85f9
Cuking01 <cuking998244353@gmail.com>
2025-05-01 21:59:06 +0800

    加点libtorch版本输出

--- test/test_libtorch_and_cuda.cpp
+++ test/test_libtorch_and_cuda.cpp
@@ -3,5 +3,6 @@
 
 int main() {
     std::cout << "CUDA available: " << torch::cuda::is_available() << std::endl;
+    std::cout << "PyTorch version: " << TORCH_VERSION_MAJOR << "." << TORCH_VERSION_MINOR << "." << TORCH_VERSION_PATCH << std::endl;
     return 0;
 }
